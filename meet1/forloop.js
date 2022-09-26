07aefd1f10fbdd48015ca1579ffed04a60977059
Mintho Siagian <mintho.siagian@dkatalis.com>
2022-09-26 23:11:26 +0700

    for loop material

--- meet1/forloop.js
+++ meet1/forloop.js
@@ -1,18 +1,18 @@
-// for(let i=0; i < 3; i++) {
-//     console.log(`i - ${i}`)
+for(let i=0; i < 3; i++) {
+    console.log(`i - ${i}`)
 
-//     for(let j=0; j < 3; j++) {
-//         console.log(`  j - ${j}`)
+    for(let j=0; j < 3; j++) {
+        console.log(`  j - ${j}`)
 
-//         for(let l=0; l < 3; l++) {
-//             console.log(`     l - ${l}`)
-//         }
-//     }
+        for(let l=0; l < 3; l++) {
+            console.log(`     l - ${l}`)
+        }
+    }
 
-//     for(let k=0; k < 3; k++) {
-//         console.log(`  k - ${k}`)
-//     }
-// }
+    for(let k=0; k < 3; k++) {
+        console.log(`  k - ${k}`)
+    }
+}
 
 // counter++ => counter+=1 => counter = counter + 1
 let sum = 0;
