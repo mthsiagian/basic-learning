4100c022ecccf472466b0a46159458c27eb6a2b7
Mintho Siagian <mintho.siagian@dkatalis.com>
2022-10-04 22:34:49 +0700

    tugas 4 oct

---
+++ tugas/tugas4.js
@@ -0,0 +1,53 @@
+// 1. FIZZBUZZ
+// Tulis sebuah program yang mengeluarkan output 1 - 100.
+// Setiap kelipatan 3 output "Fizz"
+// Setiap kelipatan 5 output "Buzz"
+// Setiap kelipatan 3 dan 5 output, "FizzBuzz"
+// Output: 1, 2, "Fizz", 4, "Buzz", 6, ..., 13, 14, "FizzBuzz", 16, ..., 98, "Fizz", "Buzz"
+
+// 2. Penjumlahan bilangan prima dengan metode rekursif
+// Contoh: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, to n
+
+// 3. Penjumlahan bilangan dari angka kelipatan 3 dan 5 sejumlah n input dimulai dari 1
+// Contoh: n = 11 
+// hasil = 3 + 5 + 6 + 9 + 10 = ....
+
+// 4. Buat sebuah program yang mengeluarkan output tabel perkalian sampai n max=12
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '\n'
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '1 x 3 = 3'
+// '\n'
+
+// 5. Buat sebuah function yang mengeluarkan output bilangan terbesar dari sebuah array
+// Contoh: Array [3, 5, 1, 2, 4, 6, 20, 22, 53, 32] 
+// Output: 53
+
+// 6. Buat sebuah function yang mengeluarkan satu output string yang memiliki length terpendek
+// Contoh: Array ["abc", "jery", "herman", "vio", "dio", "jesica", "a"]
+// Output: "a" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
