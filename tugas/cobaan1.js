42d496173f3383a88e7ac72f78eb1a1ad2bf1a76
sidabutar <napitu.ucok12@gmail.com>
2022-11-18 17:02:24 +0700

    cobaan dari Vio

--- tugas/cobaan1.js
+++ tugas/cobaan1.js
@@ -1,7 +1,14 @@
-let kata = sasagas; 
-let panjangKata = kata.lenght;
-
-for ( let i = 0; i < panjangKata; i++ ){
-    let kataBagi = kata.slice();
-    console.log(kataBagi);
+function soalVio(n) {
+    for (let i = 1; i <= n; i++) {
+        let tempat = [];
+        for (let j = 1; j <= n + 3; j++) {
+            if (j == i + 1 || j == i + 2) {
+                tempat.push("*");
+            } else {
+                tempat.push(j);
+            }
+        }
+        console.log(tempat.join(""));
+    }
 }
+soalVio(2);
