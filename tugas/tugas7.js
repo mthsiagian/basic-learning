ca4f8be0e4d6ba5dbbfa78f9a13f4bb2a68cdf21
sidabutar <napitu.ucok12@gmail.com>
2022-11-30 15:21:41 +0700

    push

--- tugas/tugas7.js
+++ tugas/tugas7.js
@@ -1,1 +1,22 @@
 // Buatlah sebuah class untuk mengggambarkan kartu joker.
+class joker {
+    #fungsi;
+    constructor(fungsi) {
+        this.#fungsi = fungsi;
+    }
+    // setfungsi() {
+    //     this.#fungsi = fungsi;
+    // }
+    // getfungsi() {
+    //    return this.#fungsi;
+    // }
+    kegunaan(){
+        if (typeof(this.#fungsi) == 'number'){
+            console.log("Kartu Anda adalah Joker !!;D ");
+        } else {
+            console.log("Maaf Kartu Anda bukan Joker !! -_-")
+        }
+    }
+}
+const angka = new joker (1);
+angka.kegunaan();
